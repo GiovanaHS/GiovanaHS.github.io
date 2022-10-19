3d919c05cbc22598a0c49b24ad41e29fb39ff392
GiovanaHS <111936956+GiovanaHS@users.noreply.github.com>
2022-10-19 19:57:34 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+
+                   
+    document.write ("Ana Beatriz<br>");
+    document.write ("Ano - 2022<br>");
+    document.write ("Twitter: @Bea5<br>");
+    document.write ("Facebook: Bea5<br>");
+    document.write ("Instagram: AnaB5<br>");
+    document.write ("Linkedin: Ana Beatriz<br>");
+    document.write ("Sexo: Feminino<br>");
+    document.write ("Localização - polo: Higienópolis");
+                  
+
+           
