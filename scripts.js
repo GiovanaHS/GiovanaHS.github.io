eaad96a01c18d2a63b4e956717967bebf68462bb
GiovanaHS <111936956+GiovanaHS@users.noreply.github.com>
2022-10-20 01:46:42 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,49 @@
+<template>
+  <v-footer
+    dark
+    padless
+  >
+    <v-card
+      flat
+      tile
+      class="indigo lighten-1 white--text text-center"
+    >
+      <v-card-text>
+        <v-btn
+          v-for="icon in icons"
+          :key="icon"
+          class="mx-4 white--text"
+          icon
+        >
+          <v-icon size="24px">
+            {{ icon }}
+          </v-icon>
+        </v-btn>
+      </v-card-text>
 
-                   
-    document.write ("Ana Beatriz<br>");
-    document.write ("Ano - 2022<br>");
-    document.write ("Twitter: @Bea5<br>");
-    document.write ("Facebook: Bea5<br>");
-    document.write ("Instagram: AnaB5<br>");
-    document.write ("Linkedin: Ana Beatriz<br>");
-    document.write ("Sexo: Feminino<br>");
-    document.write ("Localização - polo: Higienópolis");
-                  
+      <v-card-text class="white--text pt-0">
+        Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-text class="white--text">
+        {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
+</template>
+
+
+  export default {
+    data: () => ({
+      icons: [
+        'mdi-facebook',
+        'mdi-twitter',
+        'mdi-linkedin',
+        'mdi-instagram',
+      ],
+    }),
+  }
 
            
