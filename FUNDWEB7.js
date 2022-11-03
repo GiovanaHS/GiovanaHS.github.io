5c388f55792fd5ea063d741ab3c3494debe3fbfc
GiovanaHS <111936956+GiovanaHS@users.noreply.github.com>
2022-11-03 19:53:05 -0300

    Add files via upload

---
+++ FUNDWEB7.js
@@ -0,0 +1,23 @@
+var tabela=new Array();
+
+
+
+tabela[0]="<p><b>Futebol:</b> O nome do time é Corinthians, o clube conta com 11 jogadores e sua cor principal é Preto</p>";
+tabela[1]="<p><b>Basquete:</b> O nome do time é Memphis Grizzlies, o clube conta com 5 jogadores e sua cor principal é o Azul</p>";
+tabela[2]="<p><b>Futebol Americano:</b> O nome do time é Kansas City Chiefs, o clube conta com 11 jogadores e sua cor principal é o Vermelho";
+   
+            
+    document.write(tabela[0]+"<br>");
+    document.write(tabela[1]+"<br>");
+    document.write(tabela[2]+"<br>");
+
+ 
+ 
+    
+
+
+
+
+    
+  
+        
